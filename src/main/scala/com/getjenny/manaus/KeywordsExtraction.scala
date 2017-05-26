8909467a3e1a220e82218c11663c59c01a064d0e
angleto <angleto@gmail.com>
2017-05-26 21:34:34 +0200

    progress on refactoring

--- src/main/scala/com/getjenny/manaus/KeywordsExtraction.scala
+++ src/main/scala/com/getjenny/manaus/KeywordsExtraction.scala
@@ -5,9 +5,6 @@ import com.getjenny.manaus.util.Binomial
 
 /** Created by Mario Alemi on 07/04/2017 in El Estrecho, Putumayo, Peru
   *
-  *
-  *
-  *
   * conversations: A List of `String`s, where each element is a conversation.
   * tokenizer
   * priorOccurrences: A Map with occurrences of words as given by external corpora (wiki etc)
@@ -29,22 +26,14 @@ import com.getjenny.manaus.util.Binomial
       priorOccurrences=priorOccurrences)
 *```
   *
-  *
-  *
-  *
   * @param priorOccurrences Map with occurrence for each word from a corpus different from the conversation log.
   * @param exchanges list of tokenized sentences grouped by conversation
   * @param observedOccurrences occurrence of terms into the observed vocabulary
-  * @param minObservedNForPruning the min number of occurrences of the word in the corpus vocabulary
-  * @param minWordsPerSentence a sentence with less than that is not considered
-  *
   *
   */
 class KeywordsExtraction(priorOccurrences: TokensOccurrences,
                          observedOccurrences: TokensOccurrences,
-                         exchanges: List[List[(String, List[String])]],
-                         minObservedNForPruning: Int = 100000,
-                         minWordsPerSentence: Int = 10) {
+                         exchanges: List[List[(String, List[String])]]) {
 
   /**
     * @param sentence_tokens list of sentence tokens
@@ -80,8 +69,15 @@ class KeywordsExtraction(priorOccurrences: TokensOccurrences,
     }
   }
 
-  // No words with two letters, words which appear only once in the corpus (if this is big enough)
-  private def pruneSentence(sentence: List[String], min_chars: Int = 2): List[String] = {
+  /** Clean a list of tokens e.g. No words with two letters,
+    *   words which appear only once in the corpus (if this is big enough)
+    * @param sentence the list of the token of the sentence
+    * @param minObservedNForPruning the min number of occurrences of the word in the corpus vocabulary
+    * @param min_chars the min number of character for a token
+    * @return a cleaned list of tokens
+    */
+  def pruneSentence(sentence: List[String],
+                    minObservedNForPruning: Int = 100000, min_chars: Int = 2): List[String] = {
     val pruned_sentence = if (observedOccurrences.getTokenN > minObservedNForPruning)
       sentence.filter(_.length > min_chars).map(token => token.toLowerCase)
         .map(token => (token, observedOccurrences.getOccurrence(token))).filter(_._2 > 1)
@@ -90,46 +86,5 @@ class KeywordsExtraction(priorOccurrences: TokensOccurrences,
       sentence.filter(_.length > min_chars)
     pruned_sentence
   }
-
-  // Prepare sentences (each is a list of Strings)
-  val sentences: List[List[String]] = exchanges.flatMap(_.map( _._2  ))
-  val observedVocabulary: List[String] = sentences.flatten
-
-  // Because we want to check that keywords are correctly extracted,
-  //  will have tuple like (original words, keywords, bigrams...)
-  val rawBagOfKeywordsInfo: List[(List[String], List[(String, Double)])] =
-    sentences.map(x => pruneSentence(x)).filter(_.length >= minWordsPerSentence)
-      .map(x => {(x, new Sentence(x).keywords)})
-
-  val rawKeywords: List[(List[String], Set[String])] = for (l <- rawBagOfKeywordsInfo)
-    yield (l._1, (for (ki <- l._2) yield ki._1).toSet)
-
-  // Now we want to filter the important keywords. These are the ones
-  // who appear often enough not to surprise us anymore.
-  val extractedKeywords: Map[String, Double] =
-    (rawBagOfKeywordsInfo.flatMap(_._2).map(_._1) groupBy (w => w))
-      .map(p =>
-        (p._1,
-          Binomial(priorOccurrences.getTokenN + observedOccurrences.getTokenN,
-            observedOccurrences.getOccurrence(p._1.toLowerCase) + priorOccurrences.getOccurrence(p._1.toLowerCase)
-          ).activePotential(p._2.length)
-        )
-      )
-
-  //TODO temporary solution, need to understand how to set a cutoff
-  private val ekList = extractedKeywords.toList.sortBy(_._2)
-  val cutoff: Double = ekList(ekList.length/10)._2
-
-  val keywords: List[(List[String], Set[String])] =
-    rawBagOfKeywordsInfo.map(sentence => {
-      val pruned_sentence_tokens = sentence._1
-      val extracted_keywords = sentence._2.map(token => (token, extractedKeywords(token._1))).filter(_._2 < cutoff)
-        .map(x => x._1._1).toSet
-      (pruned_sentence_tokens, extracted_keywords)
-    })
-
-  //TODO :
-  // * extract keywords of conversations
-  // * extract key bi/trigrams in conversations
 
 }
--- src/main/scala/com/getjenny/manaus/commands/SampleKeywordExtraction.scala
+++ src/main/scala/com/getjenny/manaus/commands/SampleKeywordExtraction.scala
@@ -33,6 +33,7 @@ object SampleKeywordExtraction {
       split_sentences(l)
     }).filter(_.nonEmpty)
 
+    // Prepare sentences (each is a list of Strings)
     val sentences: List[List[String]] = exchanges.flatMap(_.map( _._2  ))
     val observedVocabulary: List[String] = sentences.flatten
 
@@ -41,18 +42,49 @@ object SampleKeywordExtraction {
 
     val observedOccurrences = new ObservedTokensOccurrencesMap(observedOccurrencesMap)
 
-    val conversations = new KeywordsExtraction(priorOccurrences=priorOccurrences,
+    val keywordsExtraction = new KeywordsExtraction(priorOccurrences=priorOccurrences,
       observedOccurrences=observedOccurrences,
       exchanges=exchanges)
 
-    val bags = conversations.keywords
+    val minWordsPerSentence: Int = 10 // a sentence with less than that is not considered
+    // Because we want to check that keywords are correctly extracted,
+    //  will have tuple like (original words, keywords, bigrams...)
+    val rawBagOfKeywordsInfo: List[(List[String], List[(String, Double)])] =
+      sentences.map(x => keywordsExtraction.pruneSentence(x)).filter(_.length >= minWordsPerSentence)
+        .map(x => {(x, new keywordsExtraction.Sentence(x).keywords)})
 
-    println("Raw Keywords:\n" + conversations.rawBagOfKeywordsInfo.take(100).mkString("\n"))
+    val rawKeywords: List[(List[String], Set[String])] = for (l <- rawBagOfKeywordsInfo)
+      yield (l._1, (for (ki <- l._2) yield ki._1).toSet)
 
-    println("Total Extracted Keywords: " + conversations.extractedKeywords.toList.length)
-    println("Extracted Keywords:\n" + conversations.extractedKeywords.take(500))
+    // Now we want to filter the important keywords. These are the ones
+    // who appear often enough not to surprise us anymore.
+    val extractedKeywords: Map[String, Double] =
+      (rawBagOfKeywordsInfo.flatMap(_._2).map(_._1) groupBy (w => w))
+        .map(p =>
+         (p._1,
+           Binomial(priorOccurrences.getTokenN + observedOccurrences.getTokenN,
+             observedOccurrences.getOccurrence(p._1.toLowerCase) + priorOccurrences.getOccurrence(p._1.toLowerCase)
+           ).activePotential(p._2.length)
+         )
+       )
 
-    println("Clean Keywords:\n" + conversations.keywords.take(500))
+    val ekList = extractedKeywords.toList.sortBy(_._2)
+    val cutoff: Double = ekList(ekList.length/10)._2
+
+    val bags: List[(List[String], Set[String])] =
+      rawBagOfKeywordsInfo.map(sentence => {
+        val pruned_sentence_tokens = sentence._1
+        val extracted_keywords = sentence._2.map(token => (token, extractedKeywords(token._1))).filter(_._2 < cutoff)
+          .map(x => x._1._1).toSet
+        (pruned_sentence_tokens, extracted_keywords)
+      })
+
+    println("Raw Keywords:\n" + rawBagOfKeywordsInfo.take(100).mkString("\n"))
+
+    println("Total Extracted Keywords: " + extractedKeywords.toList.length)
+    println("Extracted Keywords:\n" + extractedKeywords.take(500))
+
+    println("Clean Keywords:\n" + bags.take(500))
 
     val g = Bags(bags)
     //val g = LogLikelihoodScore(List(Set("A", "B", "C"), Set("A", "B", "D"), Set("A", "B")))
