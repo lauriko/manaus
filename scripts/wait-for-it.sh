add18865dae58b8f90df0865eeb1e5942e83a772
Angelo Leto <angleto@gmail.com>
2017-07-03 20:19:52 +0200

    added a new wait interval

--- scripts/wait-for-it.sh
+++ scripts/wait-for-it.sh
@@ -8,6 +8,9 @@ shift
 port="$1"
 shift
 
+additional_wait="$1"
+shift
+
 cmd="$@"
 
 until nc -z ${host} ${port}
@@ -15,6 +18,11 @@ do
   echo "Service is still unavailable on: ${host}:${port}"
   sleep 1
 done
+
+if [[ $additional_wait -gt 0 ]]; then
+	echo "waiting $additional_wait seconds more for the service"
+	sleep ${additional_wait}
+fi
 
 echo "Service is up - executing command"
 exec $cmd
